e6c42b2abe92f9395587969d42c9fb484887b069
shinora008 <k.shinozaki8@gmail.com>
2021-04-05 09:43:57 +0900

    fix/webhook_controller

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -8,30 +8,31 @@ class WebhookController < ApplicationController
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
+  end
 
-    def callback
-      body = request.body.read
+  def callback
+    body = request.body.read
 
-      signature = request.env['HTTP_X_LINE_SIGNATURE']
-      unless client.validate_signature(body, signature)
-        halt 400, {'Content-Type' => 'text/plain'}, 'Bad Request'
-      end
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      halt 400, {'Content-Type' => 'text/plain'}, 'Bad Request'
+    end
 
-      events = client.parse_events_from(body)
+    events = client.parse_events_from(body)
 
-      events.each do |event|
-        case event
-        when Line::Bot::Event::Message
-          case event.Type
-          when Line::Bot::Event::MessageType::text
-            message = {
-              type: 'text',
-              text: event.message['text']
-            }
-            client.reply_message(event['replyToken'], message)
-            end
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.Type
+        when Line::Bot::Event::MessageType::text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
           end
         end
+      end
 
       "OK"
     end
